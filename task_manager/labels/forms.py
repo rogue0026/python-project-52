49030f830c24441cc1683eab6511bb5cfbb4ae6c
Paul Nozdrachev <p.nozdra4ev@yandex.ru>
2025-12-14 12:43:07 +0300

    .

--- task_manager/labels/forms.py
+++ task_manager/labels/forms.py
@@ -4,6 +4,7 @@ from django import forms
 class LabelForm(forms.Form):
     name = forms.CharField(
         required=True,
+        label_suffix="",
         label="Имя",
         widget=forms.TextInput(attrs={
             "id": "id_name",
