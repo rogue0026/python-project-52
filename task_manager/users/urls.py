01d7837ec4de90d2a2ddf10b216a93bd3b267aca
Paul Nozdrachev <p.nozdra4ev@yandex.ru>
2025-12-15 10:40:39 +0300

    .

--- task_manager/users/urls.py
+++ task_manager/users/urls.py
@@ -2,10 +2,28 @@ from django.urls import path
 
 from task_manager.users import views
 
-
 urlpatterns = [
-    path("create/", views.UserRegistrationView.as_view(), name="registration_view"),
-    path("<int:pk>/update/", views.UpdateUserView.as_view(), name="users_update_view"),
-    path("<int:pk>/delete/", views.DeleteUserView.as_view(), name="users_delete_view"),
-    path("", views.UserListView.as_view(), name="users_list_view"),
+    path(
+        "create/",
+        views.UserRegistrationView.as_view(),
+        name="registration_view",
+    ),
+
+    path(
+        "<int:pk>/update/",
+        views.UpdateUserView.as_view(),
+        name="users_update_view",
+    ),
+
+    path(
+        "<int:pk>/delete/",
+        views.DeleteUserView.as_view(),
+        name="users_delete_view",
+    ),
+
+    path(
+        "",
+        views.UserListView.as_view(),
+        name="users_list_view",
+    ),
 ]
