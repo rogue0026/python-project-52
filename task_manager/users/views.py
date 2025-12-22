a837cf84b061918c042884b960e9db79fd44ca72
Paul Nozdrachev <p.nozdra4ev@yandex.ru>
2025-12-22 17:02:02 +0300

    .

--- task_manager/users/views.py
+++ task_manager/users/views.py
@@ -1,5 +1,6 @@
 from django.contrib import messages
 from django.contrib.auth.models import User
+from django.contrib.messages.views import SuccessMessageMixin
 from django.urls import reverse_lazy
 from django.views.generic.edit import (
     CreateView,
@@ -19,46 +20,40 @@ class UserListView(ListView):
     model = User
 
 
-class UserRegistrationView(CreateView):
+class UserRegistrationView(SuccessMessageMixin, CreateView):
     template_name = "users/registration.html"
     success_url = reverse_lazy("login_view")
     form_class = RegistrationForm
 
-    def form_valid(self, form):
-        messages.success(self.request, "Пользователь успешно зарегистрирован")
-        return super().form_valid(form)
+    success_message = "Пользователь успешно зарегистрирован"
 
 
 class UpdateUserView(
     AuthRequiredMixin,
     EditPermissionRequiredMixin,
+    SuccessMessageMixin,
     UpdateView):
 
     template_name = "users/update.html"
     success_url = reverse_lazy("users_list_view")
     model = User
     form_class = UserUpdateForm
-
-    def form_valid(self, form):
-        messages.success(self.request, "Пользователь успешно изменен")
-        return super().form_valid(form)
+    success_message = "Пользователь успешно изменен"
 
 
 class DeleteUserView(
     AuthRequiredMixin,
     EditPermissionRequiredMixin,
+    SuccessMessageMixin,
     DeleteView):
 
     login_url = "users/login/"
     success_url = reverse_lazy("users_list_view")
     template_name = "users/delete.html"
     model = User
+    success_message = "Пользователь успешно удален"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["user_id"] = self.request.user.id
         return context
-
-    def form_valid(self, form):
-        messages.success(self.request, "Пользователь успешно удален")
-        return super().form_valid(form)
